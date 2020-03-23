ee178e968a9f855b1bfec192381b9b09cd5a420c
Alan Nguyen <tuan8101@gmail.com>
2020-03-22 23:34:06 -0600

    Create profile iterator

--- app.js
+++ app.js
@@ -24,3 +24,15 @@ const data = [
     image: 'https://randomeuser.me/api/portraits/men/83.jpg'
   }
 ];
+// Profile Iterator
+function profileIteration(profiles) {
+  let nextIndex = 0;
+
+  return {
+    next: function() {
+      return nextIndex < profiles.length
+        ? { value: profiles[nextIndex++], done: false }
+        : { done: true };
+    }
+  };
+}
